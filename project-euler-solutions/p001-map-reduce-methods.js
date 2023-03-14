1f1f9fb20aef4ff9a70f2d42ffe04cd4f840a546
Yalê Ericssen <78110731+ericssendelima@users.noreply.github.com>
2023-03-14 08:01:55 -0300

    Update p001-map-reduce-methods.js

--- project-euler-solutions/p001-map-reduce-methods.js
+++ project-euler-solutions/p001-map-reduce-methods.js
@@ -1,8 +1,7 @@
 /* Se listarmos todos os números naturais abaixo de 10 que são múltiplos de 3 ou 5, obtemos 3, 5, 6 e 9. 
 A soma desses múltiplos é 23.
 Encontre a soma de todos os múltiplos de 3 ou 5 abaixo de 1000.*/
-function num(n){
-    let sum = 0;
+const sum = n => {
     const multiples = [];
     const arr = [];
 
@@ -15,10 +14,8 @@ function num(n){
             multiples.push(ns);
         };
     });
-    
-    sum = multiples.reduce((somatorio, atual) => {return somatorio + atual}, 0);
 
-    return sum;
+    return multiples.reduce((somatorio, atual) => {return somatorio + atual}, 0);
 };
 
-console.log(num(1000));//233168
+console.log(sum(1000)); //233168
